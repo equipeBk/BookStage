3643079aedb26e99f25a727f58826e9f70c20dcd
José Wytalo <112438631+jwapwytalo@users.noreply.github.com>
2022-11-17 00:21:19 -0300

    Mudanças
    Adicionei uma classe filha chamada de "Obra educacional", seus atributos e os respectivos gets e sets, também foi adicionado um menu temporário até que a interface seja implementada.

---
+++ BookStage/Classes/ObraEducacional.java
@@ -0,0 +1,28 @@
+package Classes;
+
+
+public class ObraEducacional extends Obra {
+
+	private String referencias;
+	
+	
+	
+	
+	public String getReferencias() {
+		return referencias;
+	}
+
+
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+
+
+	public String toString() {
+		return "ObraLiteraria []";
+	}
+	
+	
+	
+
+}
