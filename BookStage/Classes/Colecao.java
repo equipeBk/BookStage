0c8a6472a3eefa8058ecd5b9cb4c6163d488f9cd
José Wytalo <112438631+jwapwytalo@users.noreply.github.com>
2022-11-17 00:18:54 -0300

    Mudanças
    Adicionei uma classe filha chamada de "Obra educacional", seus atributos e os respectivos gets e sets, também foi adicionado um menu temporário até que a interface seja implementada.

---
+++ BookStage/Classes/Colecao.java
@@ -0,0 +1,20 @@
+package Classes;
+
+
+public interface Colecao {
+
+	void PesquisarPorDados(String pesq);
+
+	void PesquisarPreco(int precoInicial, int precoFinal);
+
+	void ImprimeEstoque();
+
+	void inserirObra();
+
+	void imprimeDadosPrincipais();
+
+	void imprimeTodosDados();
+
+	void imprimeLivroMaisVendido();
+
+}
